5d63f2b73b809a03ddddf913a2d1d8ff31c5a281
ptrebulle <pauline.trebulle@gmail.com>
2019-02-25 17:43:32 +0100

    Fix titles in data descriptions

--- R/data.R
+++ R/data.R
@@ -1,4 +1,5 @@
 #' SC_GRN_1
+#'
 #' A coregnet object infered from the m3d dataset describing the gene
 #' regulatory network for S. cerevisiae as described in Banos, D. T., Trébulle,
 #' P., & Elati, M. (2017). Integrating transcriptional activity in genome-scale
@@ -12,6 +13,7 @@
 "SC_GRN_1"
 
 #' aliases_SC
+#'
 #' A data.frame containing the gene ID used in the metabolic model and their
 #' common name, used in the gene regulatory network
 #' @format a two colums data.frame which first columns correspond to the name
@@ -20,6 +22,7 @@
 "aliases_SC"
 
 #' SC_EXP_DATA
+#'
 #' A matrix of S. cerevisiae gene expression in various experimental designs,
 #' derived from the m3d dataset to infer S. cerevisiae gene regulatory network.
 #' The dataset was shorten to 3600 genes in order to limit the size of the
@@ -28,34 +31,40 @@
 "SC_EXP_DATA"
 
 #' SC_Test_data
+#'
 #' A matrix of S. cerevisiae gene expression during diauxic shift (Brauer and
 #' al.)
 #' @format a matrix of 6028 genes by 13 samples during diauxic shift
 "SC_Test_data"
 
 #' SC_experiment_influence
+#'
 #' A vector of influence computed from the first sample of SC_Test_data
 #' @format a named numerical vector
 "SC_experiment_influence"
 
 #' iMM904
+#'
 #' A S. cerevisiae genome-scale metabolic model as a modelOrg object
 #' @format a modelOrg object as required by sybil. See sybilSBML for more
 #' information on how to load other model.
 "iMM904"
 
 #' PredictedGeneState
+#'
 #' Predicted gene state as obtained by the function
 #' predict_linear_model_influence
 #' @format a named vector
 "PredictedGeneState"
 
 #' ODcurveToMetCurve
+#'
 #' List as obtained by the function ODcurveToMetCurve
 #' @format List as obtained by the function ODcurveToMetCurve
 "ODcurveToMetCurve"
 
 #' ODtoflux
+#'
 #' List as obtained by the function ODCurveToFluxCurves
 #' @format List as obtained by the function ODcurveToMetCurve
 "ODtoflux"
